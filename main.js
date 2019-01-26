0a967190d49fc74dd68cdd2bdff205e4980953e5
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-01-26 12:19:16 -0700

    Cleanup extranous import.

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var config = require('./config');
-
 var request = require('request');
-
-var requestController = require('./controllers/requestController');
 
 var url = `https://${config.region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${config.testSummonerName}?api_key=${config.apiKey}`;
 
